07622c1c52cba6572a3b4146f8c4f4f7f3f48686
Toka Elfawey <tokaelfawey1@gmail.com>
2025-12-14 12:11:53 +0200

    available_actions.py

---
+++ available_actions.py
@@ -0,0 +1,13 @@
+def available_actions(self, current_state):
+    actions = []
+    player = self.current_player(current_state)
+    
+    for col in range(7):
+        if current_state[0][col] != NULL:
+            continue
+        for row in range(5, -1, -1):
+            if current_state[row][col] == NULL:
+                actions.append((player, row, col))
+                break
+    
+    return actions
