4a6a38b58657ad86149f4c3fa59bb7ad46f58cd3
Toka Elfawey <tokaelfawey1@gmail.com>
2025-12-14 12:17:11 +0200

    available_actions.py

--- available_actions.py
+++ available_actions.py
@@ -3,7 +3,7 @@ def available_actions(self, current_state):
     player = self.current_player(current_state)
     
     for col in range(7):
-        if current_state[0][col] != NULL:
+        if current_state[0][col] is not Null:
             continue
         for row in range(5, -1, -1):
             if current_state[row][col] == NULL:
