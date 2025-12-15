4713fc4b7082285e2b6b989e987d2145220df953
Toka Alaa <164672933+toka-alaa@users.noreply.github.com>
2025-12-16 00:06:31 +0200

    Refactor human_play to include player in action

--- humanplay_func.py
+++ humanplay_func.py
@@ -1,18 +1,18 @@
-def human_play(self, current_state):
-    self.display_grid(current_state)
-    player = self.current_player(current_state)
-    print(f"Your turn, you are playing with {player}")
+    def human_play(self, current_state):
+            self.display_grid(current_state)
+            player = self.current_player(current_state)
+            print(f"Your turn ({player})")
 
-    valid_columns = self.available_actions(current_state)
-    print(f"Available columns: {valid_columns}")
+            valid_columns = [col for (_, col) in self.available_actions(current_state)]
+            print(f"Available columns: {valid_columns}")
 
-    while True:
-        col = int(input("Choose a column (0-6): "))
-        if col in valid_columns:
-            break
-        else:
-            print("Invalid column! Try again.")
+            while True:
+                col = int(input("Choose a column (0-6): "))
+                if col in valid_columns:
+                    break
+                else:
+                    print("Invalid column! Try again.")
 
-    new_state = self.take_action(current_state, col)
-    self.display_grid(new_state)
-    return new_state
+            new_state = self.take_action(current_state, (player, col))
+            self.display_grid(new_state)
+            return new_state
